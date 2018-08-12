850198fb703dea7ac90a4c1454126e1dcc6ab07d
Anway Somani <=>
2018-08-12 16:19:34 +0530

    Performed corrections in forms.py for 'model'

--- homepage/latex/forms.py
+++ homepage/latex/forms.py
@@ -3,16 +3,16 @@ from django.db import models
 from .models import Branch, Specialization, Semester, Course, Module, Chapter, Question, Marks, Additional
 
 class InsertQuestion(forms.ModelForm):
-     branch = forms.ModelChoiceField(queryset=models.Branch.objects.all())
-     specialization = forms.ModelChoiceField(queryset=models.Specialization.objects.none())
-     chapter = forms.ModelChoiceField(queryset=models.Chapter.objects.none())
-     course = forms.ModelChoiceField(queryset=models.Course.objects.none())
-     module= forms.ModelChoiceField(queryset=models.Module.objects.none())
-     chapter = forms.ModelChoiceField(queryset=models.Chapter.objects.none())
-     question= forms.ModelChoiceField(queryset=models.Question.objects.none())
-     marks = forms.ModelChoiceField(queryset=models.Marks.objects.all())
-     priority = forms.ModelChoiceField(queryset=models.Additonal.objects.all())
-     note = forms.ModelChoiceField(queryset=models.Additonal.objects.none())
+     branch = forms.ModelChoiceField(queryset=Branch.objects.all())
+     specialization = forms.ModelChoiceField(queryset=Specialization.objects.none())
+     chapter = forms.ModelChoiceField(queryset=.Chapter.objects.none())
+     course = forms.ModelChoiceField(queryset=Course.objects.none())
+     module= forms.ModelChoiceField(queryset=Module.objects.none())
+     chapter = forms.ModelChoiceField(queryset=Chapter.objects.none())
+     question= forms.ModelChoiceField(queryset=Question.objects.none())
+     marks = forms.ModelChoiceField(queryset=Marks.objects.all())
+     priority = forms.ModelChoiceField(queryset=Additonal.objects.all())
+     note = forms.ModelChoiceField(queryset=Additonal.objects.none())
 
      class Meta:
             model = models.Chapter, models.Question, models.Marks, models.Additional
